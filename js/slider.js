07027d72a0579b08f00030887c2ecdd110786293
ksh <ksheremet@satelliz.com>
2019-02-07 13:38:58 +0200

    7.2.19

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,14 @@
     $(document).ready(function(){
       $('.header-slider').slick({
         arrows: true,
-        // dots: true,
         // nextArrow: '._nextArrow',
         // prevArrow: '._prevArrow',
+      });
+      $('.reviews-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
       });
     });

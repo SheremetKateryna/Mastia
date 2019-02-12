242028aed1c7b3d0c2eeb5c52d8f151ea3cb42d6
SheremetKateryna <44502460+SheremetKateryna@users.noreply.github.com>
2019-02-12 12:42:10 +0200

    Update dropdown.js

--- dest/js/dropdown.js
+++ dest/js/dropdown.js
@@ -2,26 +2,30 @@ $(function () {
   var $list = $('._list');
 
   $list.on('click', $('.ability_list'), function(){
-      $(this).toggleClass('ability_list__active')
-             .children('.button__dropdown').toggleClass('button__dropdown__active');
+    var $this = $(this);
+        $this.toggleClass('ability_list__active')
+             .children('.button__dropdown')
+             .toggleClass('button__dropdown__active');
 
-            if ($(this).hasClass('ability_list__active')){ 
+            if ($this.hasClass('ability_list__active')){ 
 
-              $(this).css({'margin-bottom':'0'}).children('.button__dropdown__active')
-                     .children()
-                     .removeClass('fa fa-chevron-right')
-                     .addClass('fa fa-chevron-down');
+              $this.css({'margin-bottom':'0'})
+                   .children('.button__dropdown__active')
+                   .children()
+                   .removeClass('fa fa-chevron-right')
+                   .addClass('fa fa-chevron-down');
 
-              $(this).next().css({'display':'block'});
+              $this.next().css({'display':'block'});
 
             } else {
               
-              $(this).css({'margin-bottom':'10px'}).children('.button__dropdown')
-                     .children()
-                     .removeClass('fa fa-chevron-down')
-                     .addClass('fa fa-chevron-right');
+              $this.css({'margin-bottom':'10px'})
+                   .children('.button__dropdown')
+                   .children()
+                   .removeClass('fa fa-chevron-down')
+                   .addClass('fa fa-chevron-right');
 
-              $(this).next().css({'display':'none'});
+              $this.next().css({'display':'none'});
             }
    });
 });
